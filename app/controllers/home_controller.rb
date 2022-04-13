3fb75051215847bf3eb94f1d53002897b38ac446
Andrew Nesbitt <andrewnez@gmail.com>
2022-04-13 17:47:27 +0100

    Update timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,18 +9,18 @@ class HomeController < ApplicationController
         repo: 'packages'
       },
       {
+        name: 'Timeline',
+        url: 'https://timeline.ecosyste.ms',
+        description: 'An open API service providing the timeline of over 4 Billion events for every public repo on GitHub, all the way back to 2015.',
+        icon: 'clock-history',
+        repo: 'timeline'
+      },
+      {
         name: 'Digest',
         url: 'https://digest.ecosyste.ms',
         description: 'An open API service providing digests of packages from many open source software ecosystems and registries.',
         icon: 'hash',
         repo: 'digest'
-      },
-      {
-        name: 'Timeline',
-        url: 'https://timeline.ecosyste.ms',
-        description: 'Browse the timeline of over 4 Billion events for every public repo on GitHub, all the way back to 2015. Data updated hourly from GH Archive.',
-        icon: 'clock-history',
-        repo: nil #'timeline'
       },
     ]
   end
