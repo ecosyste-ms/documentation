768887b9dfe302ca90399cd114edc4c39036ec23
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-30 12:51:08 +0100

    Custom error responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/404', to: 'errors#not_found'
+  get '/422', to: 'errors#unprocessable'
+  get '/500', to: 'errors#internal'
+
   # Defines the root path route ("/")
   root "home#index"
 end
