7e3930a1a4c022bdb7001cd0878150ac06543075
Andrew Nesbitt <andrewnez@gmail.com>
2023-02-09 14:05:06 +0000

    Redirect to remove www.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_subdomain
+
+  def redirect_subdomain
+    if request.host == 'www.ecosyste.ms'
+      redirect_to 'http://ecosyste.ms' + request.fullpath, :status => 301
+    end
+  end
 end
