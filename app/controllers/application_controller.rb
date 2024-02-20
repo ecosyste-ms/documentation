86fa3b425cf8cdd8494bf40d75d3e104b7c59e92
Andrew Nesbitt <andrewnez@gmail.com>
2024-02-20 11:18:17 +0000

    Remove redirect_subdomain method from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :redirect_subdomain
 
-  def redirect_subdomain
-    if request.host == 'www.ecosyste.ms'
-      redirect_to 'http://ecosyste.ms' + request.fullpath, :status => 301
-    end
-  end
 end
