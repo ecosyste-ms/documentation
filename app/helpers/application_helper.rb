6cec4d0d5e4b545af8a9501aa44552e0413f259e
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 14:47:27 +0000

    Add JavaScript asset linking and enhance application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,14 @@ module ApplicationHelper
   end
 
   def meta_description
-    @meta_description 
+    @meta_description ||= app_description
+  end
+
+  def app_name
+    "Ecosystems"
+  end
+
+  def app_description
+    'Tools and datasets to support, sustain, and secure critical digital infrastructure.'
   end
 end
