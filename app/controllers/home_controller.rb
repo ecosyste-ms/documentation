6e184898048b646950f19787c2b43b3dceaaa856
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-28 10:43:00 +0100

    Implement styleguide rendering for specific domain in HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    
+    if request.host == 'styleguide.ecosyste.ms'
+      render 'pages/styleguide'
+    end
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,16 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'home/index'
   end
+
+  test 'renders styleguide for styleguide.ecosyste.ms domain' do
+    get root_path, headers: { 'HTTP_HOST' => 'styleguide.ecosyste.ms' }
+    assert_response :success
+    assert_template 'pages/styleguide'
+  end
+
+  test 'renders regular index for other domains' do
+    get root_path, headers: { 'HTTP_HOST' => 'ecosyste.ms' }
+    assert_response :success
+    assert_template 'home/index'
+  end
 end
